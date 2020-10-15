26b68f7f34192e8390bea8f00c7ae3adc3b1ee5b
sam505 <mainasamuel505@gmail.com>
2020-10-15 17:42:29 +0300

    making it better

--- app.py
+++ app.py
@@ -12,20 +12,21 @@ def main():
     count = 0
     data = request.get_json()
     video = data["video_link"]
-    try:
-        cap = cv2.VideoCapture(video)
-        while cap.isOpened():
-            count += 1
-            _, frame = cap.read()
-            if not _:
-                break
+    # try:
+    cap = cv2.VideoCapture(video)
+    while cap.isOpened():
+        count += 1
+        _, frame = cap.read()
+        if not _:
+            break
 
-            frame = cv2.flip(frame, 1)
-        message = 'Video finished streaming without errors'
+        frame = cv2.flip(frame, 1)
+    message = 'Video finished streaming without errors'
 
-        cap.release()
-    except:
-        message = 'There was an error capturing the video'
+    cap.release()
+
+    # except:
+    #     message = 'There was an error capturing the video'
 
     return jsonify(student_id=data['student_id'],
                    exam_id=data['exam_id'],
